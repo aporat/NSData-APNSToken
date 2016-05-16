3c9d29998d9d1a11cbd2a2dc8923e370e46c3393
Hadar Porat <hadar.porat@realpage.com>
2016-05-16 11:42:13 -0400

    added missing nullable attributes

--- NSData+APNSToken/NSData+APNSToken.m
+++ NSData+APNSToken/NSData+APNSToken.m
@@ -1,10 +1,17 @@
 //
-//  NSData_APNSToken.m
-//  NSData+APNSToken
+// Copyright 2011-2015 Adar Porat (https://github.com/aporat)
 //
-//  Created by Adar Porat on 12/15/13.
-//  Copyright (c) 2013 Adar Porat. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #import "NSData+APNSToken.h"
 
